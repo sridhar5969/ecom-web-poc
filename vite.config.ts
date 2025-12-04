0e4b62ffe130c5fc4ddb1a0a01602b927c83f92d
Sridhar R <sridhar.r@tolaram.com>
2025-12-04 16:47:54 +0530

    feat: set base path for deployment in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => {
 	const env = loadEnv(mode, process.cwd(), '');
 
 	return {
+		base: "/",
 		plugins: [
 			react(),
 			// Bundle analyzer - only in production builds
